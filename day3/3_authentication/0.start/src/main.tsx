19473f19ec4d05109f0291616a25c4883fcceebc
Josh Low <josh.low@mudbath.com.au>
2024-07-11 10:28:11 +1000

    Don't require auth for /

--- day3/3_authentication/0.start/src/main.tsx
+++ day3/3_authentication/0.start/src/main.tsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <AuthenticationGuard component={PostBoard} />,
+        element: <PostBoard />,
       },
       {
         path: "create",
